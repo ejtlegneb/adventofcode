21e269f7f170cc98485fbeacf42bc8fadb14ea08
vincent <v.mulder@octo.nu>
2020-12-03 19:40:49 +0100

    instead of going down from right side now do %

--- day3/partone.py
+++ day3/partone.py
@@ -1,39 +1,31 @@
+import fileinput
 
 def havent_done_my_taxes_im_too_turnt_up():
 
-    with open ('tdata') as f:
+    ver_i = 0
+    hor_i = 0
+    count = 0
+
+    with open ('data') as f:
         tree_map = f.read().splitlines()
 
     tree_map = [list(x) for x in tree_map]
-    max_v = len(tree_map) -1
-    max_h = len(tree_map[0]) -1
 
-    hor_index = 0
-    ver_index = 0
-    tree_count = 0
+    while ver_i + 1 < len(tree_map):
+        hor_i += 3
+        ver_i += 1
 
-    while True:
-        if ver_index > max_v:
-            break
-        if hor_index > max_h:
-            hor_index = max_h
-
-        if tree_map[ver_index][hor_index] == '#':
-            tree_map[ver_index][hor_index] = 'X'
-            tree_count += 1
+        if tree_map[ver_i][hor_i % len(tree_map[ver_i])]=='#':
+            tree_map[ver_i][hor_i % len(tree_map[ver_i])] = 'X'
+            count += 1
         else:
-            tree_map[ver_index][hor_index] = 'O'
-        hor_index += 3
-        ver_index += 1
-
+            tree_map[ver_i][hor_i % len(tree_map[ver_i])] = 'O'
 
     for line in tree_map:
         for item in line:
             print(item, end='')
         print()
-    print(tree_count)
-
-
+    print(count)
 
 if __name__ == "__main__":
     havent_done_my_taxes_im_too_turnt_up()
